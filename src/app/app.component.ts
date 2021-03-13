551f91cc437007c85a21c8bd656d115b9496459d
Pankaj Rayal <https://github.dxc.com/>
2021-03-13 18:49:53 +0530

    Google Firebase-Image Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+} from '@angular/fire/storage';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'file-upload-demo';
+
+  basePath: string = '/images';
+  downloadedFileUrl: string = '';
+  task: AngularFireUploadTask;
+  progressValue: Observable<number>;
+
+  constructor(private fireStorage: AngularFireStorage) {}
+
+  ngOnInit() {}
+
+  async onFileChanged(event: any) {
+    console.log(event);
+    const file = event.target.files[0];
+    console.log('file found');
+
+    if (file) {
+      const filePath = `${this.basePath}/${file.name}`;
+      this.task = this.fireStorage.upload(filePath, file);
+      this.progressValue = this.task.percentageChanges();
+
+      console.log(filePath);
+
+      (await this.task).ref.getDownloadURL().then((url) => {
+        this.downloadedFileUrl = url;
+        console.log(this.downloadedFileUrl);
+      });
+    } else {
+      alert('No images selected');
+      this.downloadedFileUrl = '';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.fbConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AngularFirestore],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
